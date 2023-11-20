58183579469b9a990554d67bb86cedf5f289a108
kaitwillows <lunaeclipse7777@gmail.com>
2023-11-19 19:09:47 -0700

    char moves opposite to the mouse

--- main.py
+++ main.py
@@ -5,12 +5,20 @@ import pygame
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((200, 200))
+screenRes = (1280, 720)
+screen = pygame.display.set_mode(screenRes)
+
 clock = pygame.time.Clock()
 running = True
 dt = 0
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+camera = (0, 0)
+
+
+character = pygame.image.load('./assets/ralsei.png')
+# character = pygame.transform.scale(image, (image.get_width() * 2, image.get_height() * 2))
+
+pygame.event.set_grab(True)
 
 while running:
     # poll for events
@@ -20,10 +28,18 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill("white")
 
-    pygame.draw.circle(screen, "red", player_pos, 40)
+    # charX = screenRes[0]*.2 - pygame.mouse.get_pos()[0]*.1
+    # charY = screenRes[1]*.2 - pygame.mouse.get_pos()[1]*.1
+    charX = screenRes[0]/2 - (pygame.mouse.get_pos()[0] - screenRes[0]/2)*.5
+    charY = screenRes[1]/2 - (pygame.mouse.get_pos()[1] - screenRes[1]/2)*.5
+    print(f"x: {charX}, y: {charY}")
+    
+    charPos = (charX, charY) 
+    screen.blit(character, charPos)
 
+    '''
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
         player_pos.y -= 300 * dt
@@ -33,6 +49,7 @@ while running:
         player_pos.x -= 300 * dt
     if keys[pygame.K_d]:
         player_pos.x += 300 * dt
+    '''
 
     # flip() the display to put your work on screen
     pygame.display.flip()
