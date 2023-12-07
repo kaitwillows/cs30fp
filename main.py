6bdcc697bae2269cdc103baf71c6eec04f4e7263
kaitwillows <lunaeclipse7777@gmail.com>
2023-12-07 09:52:04 -0700

    got it started

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
-# define all constants
-class Main:
+import pygame
+
+class Constants:
     SCREEN_RES = (1280, 720)
+    BACKGROUND = (255, 255, 255)
+
+class Camera:
     
+
+class Player:
+    SPRITE = pygame.image.load("./assets/ralsei.png")
+    SIZE = (23, 43)
+    SPEED_STRAIGHT = 300
+    SPEED_DIAGONAL = 212.13
+    coordinates = [0, 0]
+    hitbox = pygame.mask.from_surface(pygame.Surface((SIZE)))
+
+class Map:
+    IMAGE = pygame.image.load("./assets/map1.png")
+    SCALE_FACTOR = 50
+    SCALED_IMAGE = pygame.transform.scale(IMAGE, IMAGE.get_width() * SCALE_FACTOR, IMAGE.get_height() * SCALE_FACTOR)
+    MASK = pygame.mask.from_surface(SCALED_IMAGE)
+    
+
+
